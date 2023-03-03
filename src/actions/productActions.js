c0435efb0d341c3b227ef4299f8bcad3b7a69ae7
hadi499 <hadi.ygcl@gmail.com>
2023-03-03 09:22:20 +0700

    repair homescreen,  productAction.js

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -8,7 +8,7 @@ import {
   PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
-export const listProducts = async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
